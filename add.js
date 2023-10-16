0ff2307763d555229892a7fa28fed55835fca534
Aamir Sohail <amircheema295@gmail.com>
2023-10-16 22:17:26 +0500

    Sum of array

--- add.js
+++ add.js
@@ -6,3 +6,19 @@ const addTwo = (a, b) => {
 
 }
 addTwo(15, 35);
+
+// Sum of an array
+let numbers = [15, 15, 25, 35];
+const sumOfArray = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+};
+
+let result = sumOfArray(numbers);
+console.log(result);
+
+        
+    
